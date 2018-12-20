ca080c7f24ad176904c3ac98b424b716fb3bb269
Paul Edwards <paul.edwards@amido.com>
2018-12-20 11:15:24 +0000

    Add simple drag and drop through web sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,104 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-import logo from './logo.svg';
 import './App.css';
 
 const socket = io.connect('http://localhost:8080/board');
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
 class App extends Component {
+  state = {
+    dragItem: null,
+    items: [
+      { content: 'Item 1', id: 1 },
+      { content: 'Item 2', id: 2 },
+      { content: 'Item 3', id: 3 }
+    ],
+  };
+
   async componentDidMount() {
-    socket.on('news', (data) => {
-      console.log('news');
-      socket.emit('my other event', { my: 'data' });
+    socket.on('handleDrag', ({ x, y }) => {
+      this.setState({
+        dragItem: { x, y },
+      })
+    });
+
+    socket.on('handleDragEnd', () => {
+      this.setState({
+        dragItem: null,
+      })
     });
   }
 
-  handleButtonClick = () => {
-    socket.emit('my other event', { payload: 'button click' });
+  onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    window.removeEventListener('mousemove', this.emitCoords);
+    socket.emit('dragEnd');
+
+    this.setState({
+      items,
+    });
+  };
+
+  onDragStart = (e) => {
+    window.addEventListener('mousemove', this.emitCoords);
+  };
+  
+  emitCoords = ({ offsetX: x, offsetY: y }) => {
+    // Throttle
+    socket.emit('drag', { x, y });
   };
 
   render() {
+    const { dragItem } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <React.Fragment>
+      {dragItem && <div className="item" style={{ position: 'absolute', left: dragItem.x, top: dragItem.y }}></div>}
+      <DragDropContext onDragEnd={this.onDragEnd} onDragStart={this.onDragStart} onDragUpdate={this.onDragUpdate}>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
           >
-            Learn React
-          </a>
-        </header>
-        <button onClick={this.handleButtonClick}>Emit</button>
-      </div>
+            {this.state.items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <li
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {item.content}
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    </React.Fragment>
     );
   }
 }
